07d6ca1158b3eff6ef8c6e2916c05ed03c5255aa
1231joe1231 <zhuangruixin1231@gmail.com>
2023-01-09 20:29:39 -0500

    Complete 112

---
+++ 112-path-sum.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root, targetSum: int) -> bool:
+        if root == None:
+            return False
+        if root.left == None and root.right == None:
+            if targetSum == root.val:
+                return True
+        return self.hasPathSum(root.left, targetSum - root.val) | self.hasPathSum(root.right, targetSum - root.val)
